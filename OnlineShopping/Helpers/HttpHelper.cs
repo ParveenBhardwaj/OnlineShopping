64753938ca80fce0188c28f61800b6df058af59b
parveen <parveen-bhardwaj@outlook.com>
2020-02-06 14:16:36 +1100

    Added a comment in httphelper

--- OnlineShopping/Helpers/HttpHelper.cs
+++ OnlineShopping/Helpers/HttpHelper.cs
@@ -16,6 +16,8 @@ namespace OnlineShopping.Helpers
         public static async Task<T> GetAsync<T>(string url)
         {
             var httpClient = new HttpClient();
+            // NOTE: In a prod ready code, this will need to handle any unexpected scenarios
+            // e.g.: If the resource is not available. Or the resource returns an unexpected response. 
             var response = await httpClient.GetAsync(new Uri(url)).Result.Content.ReadAsStringAsync();
             var result = JsonConvert.DeserializeObject<T>(response);
             return result;
